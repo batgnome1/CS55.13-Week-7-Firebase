3217753c5c680d706632a2b722b96c3e5a4bfb78
Andrew Mayfield <batgnome@gmail.com>
2025-10-10 10:48:07 -0700

    still debugging forever

--- src/app/actions.js
+++ src/app/actions.js
@@ -7,6 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // This is a Server Action
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 // Replace the function below
+
 // This is a next.js server action, which is an alpha feature, so
 // use with caution.
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
