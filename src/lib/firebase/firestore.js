a198ccdc254da476e0b47be5eece888fc23365ba
Andrew Mayfield <batgnome@gmail.com>
2025-10-10 17:22:25 -0700

    adding timestamp back as firebase loves time apparently

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -69,7 +69,7 @@ const updateWithRating = async (
 
   transaction.set(newRatingDocument, {
     ...review,
-  //  timestamp: Timestamp.fromDate(new Date()),
+    timestamp: Timestamp.fromDate(new Date()),
   });
 };
 
