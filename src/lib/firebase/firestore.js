82108263e9384a928810b582d3dc164b9a6c6895
Andrew Mayfield <batgnome@gmail.com>
2025-10-10 17:30:05 -0700

    firebase is mad, trying something else

--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -73,6 +73,7 @@ const updateWithRating = async (
   });
 };
 
+
 /**
  * Add a review to a restaurant and update its rating statistics
  * This function is currently a placeholder and not implemented
